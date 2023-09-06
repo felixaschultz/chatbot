001f775f465caf88fa9dc25f2e53a57980175076
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-06 16:59:00 +0200

    Added and styled login screen also for none Intastellar Accounts user

--- login.php
+++ login.php
@@ -3,8 +3,9 @@
     <form id="login-form" method="post" class="login_form">
         <label for="email">E-Mail</label>
         <input class="input-field" type="email" name="email" id="email">
-        <button type="submit">Login</button>
+        <button class="cta --login" type="submit">Login</button>
     </form>
+    <p>or</p>
     <div id="login" data-client_id="d2eefd7f1564fa4c9714000456183a6b0f51e8c9519e1089ec41ce905ffc0c453dfac91ae8645c41ebae9c59e7a6e5233b1339e41a15723a9ba6d934bbb3e92d" data-app-name="Chatbot" data-login_callback="login"></div>
 </section>
 <script>
